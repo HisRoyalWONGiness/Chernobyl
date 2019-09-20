46ebf4c5eb98bd294cb7bffcd0c1bfbc0b69d7a8
HisRoyalWONGiness <40678152+HisRoyalWONGiness@users.noreply.github.com>
2019-09-20 22:02:41 +0100

    Update login.php

--- login.php
+++ login.php
@@ -13,16 +13,17 @@
 		<h1>Sign In</h1>
 		<?php include('errors.php'); ?>
 	  	<form method="post" action="">
-	  		<p><input type="text" name="username" value="" placeholder="Username"></p>
+	  		<p><input type="text" name="login" value="" placeholder="Username" required></p>
 	    	<p><input type="password" name="password" value="" placeholder="Password" required></p>
       		<label>
         	<input type="checkbox" name="remember_me" id="remember_me"> Keep Me Signed In </label><br>
       		<a href="#">Forgot Password?</a>.
 	    	<button class="submit" type="submit" name="login_user">SIGN IN</button>
 	  	</form>
-	 	<a href="signup.php" class="createAccount">Create Account</a>
+		<div><p>Don't Have an Account?</p></div>
+	 	<a href="signup.php" class="signup-button">Create Account</a>
 	</div>
-	<div class="login-help">
+	<div class="register">
 	</div>
 </body>
 </html>
